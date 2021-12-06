f8d99565e4e61361c59f590cb2acfdcc4672e279
data2 <muskteerathos@gmail.com>
2021-12-06 10:37:10 +0800

    消息队列 redis

--- src/main/java/com/data2/coding4j/juc/produceconsumer/RedisPublishChannelSubscribe.java
+++ src/main/java/com/data2/coding4j/juc/produceconsumer/RedisPublishChannelSubscribe.java
@@ -1,4 +1,8 @@
 package com.data2.coding4j.juc.produceconsumer;
 
 public class RedisPublishChannelSubscribe {
+
+    // subscribe channel1
+    // publish channel1 "test"
+
 }
--- src/main/java/com/data2/coding4j/juc/produceconsumer/RedisQueue.java
+++ src/main/java/com/data2/coding4j/juc/produceconsumer/RedisQueue.java
@@ -1,4 +1,5 @@
 package com.data2.coding4j.juc.produceconsumer;
 
 public class RedisQueue {
+    // lpush rpop
 }
